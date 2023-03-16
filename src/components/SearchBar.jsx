48b8da2ba3d34b7c3c47d2c0605dbbf41135ceee
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-03-16 23:41:44 +0200

    .

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,22 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import { Formik, Field, Form } from 'formik';
 
 export const SearchBar = () => {
   return (
-    <div>
-      <form>
-        <input />
-        <button>Search</button>
-      </form>
-    </div>
+    <Formik
+      initialValues={{
+        name: '',
+      }}
+      onSubmit={async values => {
+        await new Promise(r => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <Form>
+        <label htmlFor="name">Search</label>
+        <Field id="name" name="name" placeholder="Pulp fiction" type="text" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
